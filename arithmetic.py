2e7717ee803f332ab5f870d158054a1d520f5c4c
KeerthiSR01 <keerthisr0102@gmail.com>
2025-11-29 19:35:56 +0530

    Added arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+a = 30
+b = 4
+
+print (a + b) #addition
+print (a - b) #subtraction
+print (a * b) #multiplication
+print (a / b) #division
+print (a // b) #floor value 
+print (a % b) #modulus
+print (a ** b) #exponentiation
+
+print (a+b*2/4**a) #BODMAS
+
+print(4 ** 30)
+print(2 / 11529)
+print (4 * 0.000173)
