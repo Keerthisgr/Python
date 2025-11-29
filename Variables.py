0c9298f40242f1120718008babcd029707e1fb3a
KeerthiSR01 <keerthisr0102@gmail.com>
2025-11-29 19:26:15 +0530

    Added Variables.py

---
+++ Variables.py
@@ -0,0 +1,21 @@
+a = 10
+print(a)
+print(type(a))
+a = float(a)
+print(a)
+print(type(a))
+strExample="Keerthi"
+print(strExample)
+print(type(strExample))
+is_student=False
+print(type(is_student))
+c="100"
+print(int(c)+a)
+print(int(c)+int(a))
+ab = bc = cd = 100
+print(ab)
+print(cd)
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
