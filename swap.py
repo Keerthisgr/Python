198301476f6b3f1a4d8cc28be1bd2eeb50873b86
KeerthiSR01 <keerthisr0102@gmail.com>
2025-11-30 20:10:18 +0530

    added swap.py

---
+++ swap.py
@@ -0,0 +1,22 @@
+a= 10
+b= 20
+
+print("a: "+str(a))
+print("b: "+str(b))
+
+a, b = b, a
+
+print("a: "+str(a))
+print("b: "+str(b))
+
+a= 10
+b= 20
+
+
+
+a = a + b
+b = a - b
+a = a - b
+
+print("a: "+str(a))
+print("b: "+str(b))
